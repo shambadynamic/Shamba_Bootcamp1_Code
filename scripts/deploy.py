511068c8c71286dbf2fe56b592219504b6c1a85d
SyedAsadKazmi <syedasadkazmi786@gmail.com>
2022-05-21 18:26:58 +0530

    Initial commit

--- scripts/deploy.py
+++
@@ -1,8 +0,0 @@
-from brownie import MyFirstContract, config, accounts
- 
-def deployContract():
-    account = accounts.add(config["wallets"]["from_key"]) or accounts[0]
-    MyFirstContract.deploy({'from': account})
- 
-def main():
-    deployContract()
--- scripts/verify.py
+++
@@ -1,5 +0,0 @@
-from brownie import MyFirstContract
-
-def main():
-    erc20Basic = MyFirstContract[-1]    
-    MyFirstContract.publish_source(erc20Basic)
